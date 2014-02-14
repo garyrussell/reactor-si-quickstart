ba76a1abd7f6a49e43c748fb701c6828e7e9ace4
Gary Russell <grussell@gopivotal.com>
2014-02-14 13:34:08 -0500

    Add SI Adapter Infrastructure
    Add spring profiles 'reactor' and 'si' to choose.

--- src/main/java/org/projectreactor/qs/QuickStartApplication.java
+++ src/main/java/org/projectreactor/qs/QuickStartApplication.java
@@ -1,17 +1,19 @@
 package org.projectreactor.qs;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
+import org.springframework.integration.config.EnableIntegration;
 
 /**
  * Simple Spring Boot application to demonstrate integration Reactor with Spring Integration.
  *
  * @author Jon Brisbin
  */
+@EnableIntegration
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
